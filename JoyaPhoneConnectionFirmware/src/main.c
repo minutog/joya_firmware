0b8f7d5b7123eb12c3f2504ed2bc391e037706e3
fran <fspaltro@fi.uba.ar>
2026-07-24 11:02:01 +0200

    feat: code for ota capacity - tested on sdk

--- JoyaPhoneConnectionFirmware/src/main.c
+++ JoyaPhoneConnectionFirmware/src/main.c
@@ -1,10 +1,12 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/dfu/mcuboot.h>
 #include "button.h"
 #include "app_state.h"
 #include "debug_rtt.h"
 
 int main(void) {
+    bool ota_test_boot = !boot_is_img_confirmed();
     
     int ret = storage_init();
     if (ret != 0) {
@@ -17,21 +19,27 @@ int main(void) {
         dbg_rtt_mark("BLE driver initialization failed\n");
         return -1;
     }
+    ble_start_setup_advertising();
 
+    if (ota_test_boot) {
+        ble_start_setup_advertising();
+    }
+    
     ret = button_init();
     if (ret != 0) {
         dbg_rtt_mark("Button initialization failed\n");
         return -1;
     }
 
-    /*
+
     ret = haptics_init();
     if (ret < 0) {
         dbg_rtt_mark("Haptics initialization failed - continuing without haptics\n");
         // (improvement): decide what to do if haptics initialization fails (e.g., retry, log error, etc.)
-    }*/
+    }
     
     dbg_rtt_mark("Initialization complete\n");
+
 
     // Keep the main thread alive
     k_sleep(K_FOREVER);
